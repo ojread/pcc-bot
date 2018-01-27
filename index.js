59c1c8ba8b1aea23b0c68de52db15b9e37508b15
Ollie Read <oliver@oliverread.co.uk>
2018-01-27 17:05:23 +0000

    express server

--- index.js
+++ index.js
@@ -1,15 +1,24 @@
-/*
-* HTTP Cloud Function.
-*
-* @param {Object} req Cloud Function request context.
-* @param {Object} res Cloud Function response context.
-*/
-exports.helloHttp = function helloHttp (req, res) {
-  response = "This is a sample response from your webhook!" //Default response from the webhook to show it's working
+'use strict';
+
+const express = require('express');
+const bodyParser = require('body-parser');
+var request = require('request');
+const app = express();
+
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+app.use(bodyParser.json());
+
+app.post('/', function(req, res) {
+  res.json({
+    message: 'test'
+  });
+});
 
 
-  res.setHeader('Content-Type', 'application/json'); //Requires application/json MIME type
-  res.send(JSON.stringify({ "speech": response, "displayText": response 
-  //"speech" is the spoken version of the response, "displayText" is the visual version
-  }));
-};
+
+app.listen((process.env.PORT || 8000), function() {
+    console.log("Server up and listening");
+});
