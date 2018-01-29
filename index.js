fa26c4ef86b5c14a6607a742132462b1ddcfe574
Ollie Read <oliver@oliverread.co.uk>
2018-01-29 10:43:25 +0000

    log request

--- index.js
+++ index.js
@@ -12,48 +12,17 @@ app.use(bodyParser.urlencoded({
 app.use(bodyParser.json());
 
 // Show some help if someone visits the API.
-app.get('/', (req, res) => res.send("There's nothing for you here!"));
+app.get('/', (req, res) => res.send("There's nothing for you here."));
 
 // Deal with posts to the API.
-
-app.post('/weather', function(req, res) {
-  res.json({
-    fulfillmentText: 'Weather webhook response',
-  });
-});
-
+// Dialog flow sends all requests here, not to different paths.
 app.post('/', function(req, res) {
+  console.log(req);
+  
   res.json({
     fulfillmentText: 'Test webhook response',
   });
 });
-
-
-
-
-// app.post('/weather', function(req, res) {
-//   res.json({
-//     'fulfillmentText': 'fulfillmentText',
-//     // 'fulfillmentMessages': [
-//     //   {
-//     //     object(Message)
-//     //   }
-//     // ],
-//     'source': 'source',
-//     // 'payload': {
-//     //   object
-//     // },
-//     // 'outputContexts': [
-//     //   {
-//     //     object(Context)
-//     //   }
-//     // ],
-//     // 'followupEventInput': {
-//     //   object(EventInput)
-//     // },
-//   }
-//   );
-// });
 
 // Start the server.
 app.listen((process.env.PORT || 8000), function() {
