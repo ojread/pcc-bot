4f50af4ef4ddf4383239aa59de80c5dd77ed132e
Ollie Read <oliver@oliverread.co.uk>
2018-01-29 09:32:24 +0000

    try again

--- index.js
+++ index.js
@@ -16,13 +16,10 @@ app.get('/', (req, res) => res.send("There's nothing for you here!"));
 
 // Deal with posts to the API.
 app.post('/', function(req, res) {
-  response = "This is a sample response from your webhook!" //Default response from the webhook to show it's working
 
   res.setHeader('Content-Type', 'application/json'); //Requires application/json MIME type
   res.send(JSON.stringify({
-    "speech": response,
-    "displayText": response 
-    //"speech" is the spoken version of the response, "displayText" is the visual version
+    'fulfillmentText': 'Test webhook response',
   }));
 });
 
