d17649e0c16d4ec535ba3a30fbb12c66e6de4627
Ollie Read <oliver@oliverread.co.uk>
2018-01-29 09:27:22 +0000

    Google response

--- index.js
+++ index.js
@@ -16,7 +16,14 @@ app.get('/', (req, res) => res.send("There's nothing for you here!"));
 
 // Deal with posts to the API.
 app.post('/', function(req, res) {
-  res.send('Test response from webhook');
+  response = "This is a sample response from your webhook!" //Default response from the webhook to show it's working
+
+  res.setHeader('Content-Type', 'application/json'); //Requires application/json MIME type
+  res.send(JSON.stringify({
+    "speech": response,
+    "displayText": response 
+    //"speech" is the spoken version of the response, "displayText" is the visual version
+  }));
 });
 
 // app.post('/weather', function(req, res) {
